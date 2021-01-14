af4019e4ca5745e0e3b03f938251fa85f1e21102
Marcos-SP <74672261+Marcos-SP@users.noreply.github.com>
2021-01-14 16:55:06 -0300

    Exercicio 2

--- Lista1MySQL/Ecommerce.sql
+++ Lista1MySQL/Ecommerce.sql
@@ -9,6 +9,7 @@ brand varchar(200),
 size varchar(200),
 primary key(id)
 );
+
 insert into ecommerce(nome,stock,price,brand,size) values ("Tesla model S", 10,500.000, "Tesla","Médio");
 insert into ecommerce(nome,stock,price,brand,size) values ("Tesla model 3", 10,200.000, "Tesla","Médio");
 insert into ecommerce(nome,stock,price,brand,size) values ("Tesla model X", 10,600.000, "Tesla","Grande");
@@ -17,6 +18,7 @@ insert into ecommerce(nome,stock,price,brand,size) values ("Ford Ranger", 10,200
 insert into ecommerce(nome,stock,price,brand,size) values ("Audi Q8", 10,500.000, "Audi","Grande");
 insert into ecommerce(nome,stock,price,brand,size) values (" MINI Cooper S 2.0", 5,200.000, "Cooper","Pequeno");
 insert into ecommerce(nome,stock,price,brand,size) values ("Ford Mustang", 2,500.000, "Ford","Médio");
+
 select nome from ecommerce
 where price >= 500.000;
 select nome from ecommerce
